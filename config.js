e7995a4807b3bc6d29aaec77d878db7fc294ef28
slugmund <67324862+slugmund@users.noreply.github.com>
2020-06-23 09:16:10 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'LBCLbn5Vyz0kQF6rXDYVnuWwd',
-    consumer_secret: 'Bjlh0QcYFmVVPYMKJWL5r8XMIzA9HkX3tc1APCtWakx5SSmTOu',
-    access_token: '1275107122829758464-lfmnRys0GiLBrl4SYCkoNdVl47KxBU',
-    access_token_secret: 'diWPaSQPm1C1dVxZ1Pp1eilVm7nYXINNFMEK3MykmA0yI'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
     }
